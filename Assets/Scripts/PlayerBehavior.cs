e2b611b94123a4903b40c6659d5f9fdee120c4b7
samuelpereira7 <samuelpereira@gec.inatel.br>
2020-08-30 23:26:12 -0300

    add horizontal speed to the player

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    // reference to the Rigidbody component
+    private Rigidbody2D rb;
+
+    [Tooltip("horizontal speed of the player")]
+    public float horizontalSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var force = new Vector2(horizontalSpeed, 0);
+
+        rb.AddForce(force);
+        
+    }
+}
