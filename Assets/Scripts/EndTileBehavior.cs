377db12d70d5d51ec6156749394f8312050c6d67
samuelpereira7 <samuelpereira@gec.inatel.br>
2020-09-02 20:06:54 -0300

    creating tiles along the way

---
+++ Assets/Scripts/EndTileBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTileBehavior : MonoBehaviour
+{
+    [Tooltip("time waited to destroy BasicTile")]
+    public float timeDestroy = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // check if the player has passed through the end of the BasicTile
+        if (other.GetComponent<PlayerBehavior>())
+        {
+            GameObject.FindObjectOfType<GameController>().SpawnNextTile();
+
+            // destroy the BasicTile
+            Destroy(transform.parent.gameObject, timeDestroy);
+        }
+    }
+}
