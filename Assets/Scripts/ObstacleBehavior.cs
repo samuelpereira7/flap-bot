03b8e13c6fc8ce6fab5a0384459f2be5f4be23b4
samuelpereira7 <samuelpereira@gec.inatel.br>
2020-09-02 21:23:48 -0300

    create obstacle and restart the game when the player collides to it

---
+++ Assets/Scripts/ObstacleBehavior.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObstacleBehavior : MonoBehaviour
+{
+    [Tooltip("time to wait before the game starts")]
+    public float waitingTime = 2.0f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<PlayerBehavior>())
+        {
+            Destroy(collision.gameObject);
+            Invoke("ResetGame", waitingTime);
+        }
+    }
+
+    /// <summary>
+    ///  Restart the game
+    /// </summary>
+    void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        /*
+        var gameOverMenu = GetGameOverMenu();
+        gameOverMenu.SetActive(true);
+
+        var botoes = gameOverMenu.transform.GetComponentsInChildren<Button>();
+        Button botaoContinue = null;
+
+        foreach (var botao in botoes)
+        {
+            if (botao.gameObject.name.Equals("BotaoContinuar")) ;
+            {
+                botaoContinue = botao;
+                break;
+            }
+        }
+
+        if (botaoContinue)
+        {
+            StartCoroutine(ShowContinue(botaoContinue));
+
+            botaoContinue.onClick.AddListener(UnityAdControle.ShowRewardAd);
+            UnityAdControle.obstaculo = this;
+        }
+
+        // reinicia o jogo
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        */
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
